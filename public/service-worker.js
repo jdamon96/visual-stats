d1d05ebf0ba6ad612e9119fa6b62c8d13a227abc
Jack Damon <jdamon96@gmail.com>
2024-03-08 16:54:51 -0500

    first commit - working react app as a chrome extension in the sidepanel

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const BBALL_REF_ORIGIN = "https://www.basketball-reference.com";
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on google.com
+  if (url.origin === BBALL_REF_ORIGIN) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: "index.html",
+      enabled: true,
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false,
+    });
+  }
+});

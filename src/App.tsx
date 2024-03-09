73633be33ef5cc23a0e0e3a7cd5b77bedc88bb3c
Jack Damon <jdamon96@gmail.com>
2024-03-08 19:23:21 -0500

    its working :')

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { LineChart } from "@tremor/react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function App() {
   interface ChartData {
@@ -83,9 +91,6 @@ function App() {
     }
   }, [careerStatsChartData]);
 
-  const dataFormatter = (number: number) =>
-    `${Intl.NumberFormat("us").format(number).toString()}`;
-
   return (
     <div className="card">
       <button onClick={getCareerStats}>Show Career Stats on Chart</button>
@@ -94,14 +99,30 @@ function App() {
       ) : (
         careerStatsChartData !== null && (
           <LineChart
-            className="h-96"
+            width={500}
+            height={300}
             data={careerStatsChartData}
-            index="date"
-            categories={["Points", "Assists", "Rebounds"]}
-            colors={["indigo", "rose", "green"]}
-            valueFormatter={dataFormatter}
-            yAxisWidth={60}
-          />
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="points"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="assists" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="rebounds" stroke="#82ca9d" />
+          </LineChart>
         )
       )}
     </div>

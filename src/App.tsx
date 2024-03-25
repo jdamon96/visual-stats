111c7646f3f140a47e555f27c40ff0fa2e79a61c
Jack Damon <jdamon96@gmail.com>
2024-03-25 17:20:19 -0400

    changed legend styles to grid instead of flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -426,7 +426,8 @@ function App() {
       const { payload } = props;
 
       return (
-        <div className="flex w-full items-center justify-center space-x-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-items-center">
+          {" "}
           {payload.map(
             (
               entry: {
